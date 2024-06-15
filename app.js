13cceabb03cd12181a77fae6addb8268b27b8bc0
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-06-14 21:24:43 -0400

    setup skeleton express server

---
+++ app.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import path from 'path';
+import session from 'express-session';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+const mongoDB = process.env.MONGODB_URI;
+async function main() {
+    await mongoose.connect(mongoDB);
+}
+main().catch((err) => {console.log(err)});
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// TO-DO: allow lists on cors?
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.listen(PORT, ()=> {
+    console.log('app listening on port 3000!');
+});
+
+app.on('error', onError);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+  
+    const bind = typeof PORT === 'string'
+      ? 'Pipe ' + PORT
+      : 'Port ' + PORT;
+  
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
