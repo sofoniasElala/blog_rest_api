f98d9e1c06911443b3d31c3a4f4ce3e3c60feb08
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-06-14 21:23:31 -0400

    update post schema

--- models/post.js
+++ models/post.js
@@ -1,10 +1,12 @@
-import mongoose from "mongoose";
+import mongoose, { SchemaType } from "mongoose";
 const Schema = mongoose.Schema;
 const PostSchema = new Schema({
     author: {type: Schema.Types.ObjectId},
     image: {type: String},
+    title: {type: String, required: true},
     text: {type: String, required: true},
     date: {type: Schema.Types.Date},
+    published: {type: Schema.Types.Boolean},
     tag: [{type: Schema.Types.ObjectId}]
 });
 const PostModel = mongoose.model('posts', PostSchema);
