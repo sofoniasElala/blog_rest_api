d6b40c86443a3d9e42ce7a56690af13ae02ebc8b
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-06-16 03:22:39 -0400

    update max length of username property

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ main().catch((err) => {console.log(err)});
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// TO-DO: allow lists on cors?
+// TODO: allow lists on cors?
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
-    username: {type: String, required: true, minLength: 3, maxLength: 10},
+    username: {type: String, required: true, minLength: 3, maxLength: 20},
     password: {type: String, required: true},
     roles: [{type: String}]
 });
