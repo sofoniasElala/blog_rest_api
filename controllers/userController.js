db65d4c4f3cd2aceb277f99c4e80135a65c3fcf6
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-06-16 03:21:32 -0400

    add update user route handler

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,23 @@ import asyncHandler from 'express-async-handler';
 import {body, validationResult} from 'express-validator';
 import bcrypt from 'bcryptjs';
 
+
+const validationAndSanitationMiddlewareFns = [
+    body('username').trim().isLength({ min: 3, max: 20 }).escape().custom(asyncHandler(async value => {
+        const userExists = await User.find({'username': value}).exec();
+        if (userExists.length !== 0) {
+          throw new Error('Username already in use');
+        }
+    })),
+    body('password').trim().escape().isLength({ min: 5 }).withMessage('Username must be at least 5 characters long'), 
+    body('passwordConfirmation').trim().escape().custom((value, { req }) => {
+    return value === req.body.password;
+    }).withMessage('Passwords must match'), 
+    body('roles.*').escape(),
+]
+
+//TODO: GET list of all users for admin - protected route?
+
 // GET:  detail page for a specific user.
 export const user_detail = asyncHandler(async (req, res, next) => {
     const [user, allPostByUser] = await Promise.all([
@@ -17,17 +34,7 @@ export const user_detail = asyncHandler(async (req, res, next) => {
 
 // POST: create user
 export const user_create = [
-    body('username').trim().isLength({ min: 3, max: 10 }).escape().custom(asyncHandler(async value => {
-        const userExists = await User.find({'username': value}).exec();
-        if (userExists.length !== 0) {
-          throw new Error('Username already in use');
-        }
-    })),
-    body('password').trim().escape().isLength({ min: 5 }).withMessage('Username must be at least 5 characters long'), 
-    body('passwordConfirmation').trim().escape().custom((value, { req }) => {
-    return value === req.body.password;
-    }).withMessage('Passwords must match'), 
-    body('roles.*').escape(),
+    ...validationAndSanitationMiddlewareFns,
     asyncHandler(async (req, res, next) => {
         const errors = validationResult(req);
         if(!errors.isEmpty()) {
@@ -44,10 +51,30 @@ export const user_create = [
 
 ];
 
-//TODO: UPDATE user
+//PUT: update user
+export const user_update = [
+    ...validationAndSanitationMiddlewareFns,
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            res.status(400).json({sanitizedInputs: req.body, errors: errors})
+        } else {
+            const originalUserData = await User.findOne({_id: req.params.userid}, 'password').exec();
+            const updatedPassword = bcrypt.compare(req.body.password, originalUserData.password) ? originalUserData.password : await bcrypt.hash(req.body.password, 10);
+            const user = new User({
+                _id: req.params.userid,
+                username: req.body.username, 
+                password:  updatedPassword,
+                roles: req.body.roles //TODO: make sure its the correct type
+            });
+            const updatedUser = await User.findByIdAndUpdate(req.params.userid, user, {new: true})
+            res.status(200).json({id: updatedUser._id});
+        }
+    })
+];
 
 //DELETE: delete user
 export const user_delete = asyncHandler(async (req, res, next) => {
-    const deleteResult =  User.findByIdAndDelete(req.params.userid).exec();
-    res.status(200).json({result: deleteResult })
+    await User.findByIdAndDelete(req.params.userid);
+    res.status(200).json(null);
 })
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,26 @@
 import express from 'express'
 import * as user_controller from '../controllers/userController.js';
+import * as post_controller from '../controllers/postController.js';
+
+//TODO: protect all routes outside of home?
 
 const router = express.Router();
 
-router.get('/users/:userid', user_controller.user_detail);
+router.get('/users/:userid', user_controller.user_detail); //TODO: protect route
+router.put('/users/:userid', user_controller.user_update);
 
 router.delete('/users/:userid', user_controller.user_delete);
 
 router.post('/sign-up', user_controller.user_create);
+
+router.get('/', post_controller.post_list);
+
+router.post('/posts', post_controller.post_create); //TODO: protect route
+
+router.get('/posts/:postid', post_controller.post_detail);
+
+router.put('/posts/:postid', post_controller.post_update);
+
+router.delete('/posts/:postid', post_controller.post_delete); 
 
 export default router;
