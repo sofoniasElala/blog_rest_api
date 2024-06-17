ef61439af71420662f2a50d8766fb05310290520
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-06-17 02:00:00 -0400

    update schema

--- models/tag.js
+++ models/tag.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 const TagSchema = new Schema({
-    name: {type: String}
+    name: {type: String, required: true, minLength: 1}
 });
 const TagModel = mongoose.model('Tag', TagSchema);
 
