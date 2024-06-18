06b4edaf6989970d25c96c8882582e093d65f5f3
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-06-18 05:53:04 -0400

    add passport with jwt

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import path from 'path';
-import session from 'express-session';
 import cors from 'cors';
+import {Strategy as JWTStrategy} from 'passport-jwt';
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
 import mongoose from 'mongoose';
 import indexRouter from './routes/index.js';
 import userRouter from './routes/user.js';
 import tagRouter from './routes/tag.js';
 import postRouter from './routes/post.js';
+import User from './models/user.js';
 import 'dotenv/config';
 
 const mongoDB = process.env.MONGODB_URI;
@@ -21,9 +23,40 @@ const PORT = process.env.PORT || 3000;
 // TODO: allow lists on cors?
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
 
+const cookieExtractor = function(req) {
+    let token = null;
+    if (req && req.cookies) {
+        token = req.cookies['jwt'];
+    }
+    console.log(token);
+    return token;
+};
+const options = {
+    jwtFromRequest: cookieExtractor,
+    secretOrKey: process.env.ACCESS_TOKEN_SECRET,
+  };
 
+
+  passport.use(new JWTStrategy(options, async function(jwt_payload, done) {
+
+    console.log(jwt_payload);
+    try {
+        // Example using async/await
+        const user = await User.findById({ _id: jwt_payload.sub }).exec();
+
+        if (!user) {
+            return done(null, false); // User not found, authentication fails
+        }
+
+        return done(null, user); // User found, authentication succeeds
+    } catch (error) {
+        return done(error, false); // Handle any errors
+    }
+    
+}));
 
 app.use('/posts', postRouter);
 app.use('/users', userRouter);
@@ -31,35 +64,5 @@ app.use('/tags', tagRouter);
 app.use('/', indexRouter);
 
 app.listen(PORT, ()=> {
-    console.log('app listening on port 3000!');
+    console.log(`app listening on port ${PORT}!`);
 });
-
-/** app.on('error', onError);
-
-**
- * Event listener for HTTP server "error" event.
- *
-
-function onError(error) {
-    if (error.syscall !== 'listen') {
-      throw error;
-    }
-  
-    const bind = typeof PORT === 'string'
-      ? 'Pipe ' + PORT
-      : 'Port ' + PORT;
-  
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-      case 'EACCES':
-        console.error(bind + ' requires elevated privileges');
-        process.exit(1);
-        break;
-      case 'EADDRINUSE':
-        console.error(bind + ' is already in use');
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
-  } */
